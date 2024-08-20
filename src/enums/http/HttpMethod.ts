d62246c4ce37a074d8a101fcfb92adb32918db34
sinhpv <phamsinh1512@gmail.com>
2024-08-20 22:57:24 +0700

    add structure enums

---
+++ src/enums/http/HttpMethod.ts
@@ -0,0 +1,17 @@
+// src/enums/http/HttpMethods.ts
+
+/**
+ * Enum representing common HTTP methods.
+ * This enum provides a type-safe way to refer to HTTP methods used in requests.
+ */
+export enum HttpMethods {
+    GET = 'GET',
+    POST = 'POST',
+    PUT = 'PUT',
+    DELETE = 'DELETE',
+    PATCH = 'PATCH',
+    OPTIONS = 'OPTIONS',
+    HEAD = 'HEAD',
+    TRACE = 'TRACE',
+    CONNECT = 'CONNECT'
+}
---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+// import { createRouter, createWebHistory } from 'vue-router';
+
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'Home',
+//     component: () => import('@/views/Home.vue'), // Lazy load Home component
+//   },
+//   {
+//     path: '/about',
+//     name: 'About',
+//     component: () => import('@/views/About.vue'), // Lazy load About component
+//   },
+//   // More routes...
+// ];
+
+// const router = createRouter({
+//   history: createWebHistory(),
+//   routes,
+// });
+
+// export default router;
