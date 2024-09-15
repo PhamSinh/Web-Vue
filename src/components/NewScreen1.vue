8387490a97c70c3a15f31ede13a63f0061e79f72
sinhpv4 <phamsinh1512@gmail.com>
2024-09-15 12:34:01 +0700

    add source demo

---
+++ src/components/NewScreen1.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title>
+        <span class="headline">New Screen 1</span>
+      </v-card-title>
+      
+      <!-- Content -->
+      
+    </v-card>
+    <!-- Dialog for editing employee details -->
+  </v-container>
+</template>
+
+
+<script setup lang="ts">
+
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/NewScreen2.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title>
+        <span class="headline">New Screen 2</span>
+      </v-card-title>
+      
+      <!-- Content -->
+      
+    </v-card>
+    <!-- Dialog for editing employee details -->
+  </v-container>
+</template>
+
+
+<script setup lang="ts">
+
+</script>
+<style lang="scss" scoped>
+
+</style>
